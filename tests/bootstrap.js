e44cbf3746ea4c232e5bff2f52ae79b228e416e0
Nicolas Deveaud <nicolas.deveaud@francetv.fr>
2014-07-18 19:35:16 +0200

    set basic tests

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -2,8 +2,22 @@ define(['../bootstrap', 'chai', 'sinon'], function(jqpBootstrap, chai, sinon) {
     'use strict';
 
     describe('jqp-bootstrap', function() {
-        it ('should be available', function() {
-            chai.expect(jqpBootstrap).to.eql({jqp:'module'});
+        it ('should should do staticmd5 requests', function(done) {
+            jqpBootstrap.requestStaticmd5Url(null, function(error, jqpUrl) {
+                if (error) {
+                    return done(error);
+                }
+                done();
+            });
+        });
+
+        it ('should load JQP', function(done) {
+            jqpBootstrap.load(null, function(error, JQP) {
+                if (error) {
+                    return done(error);
+                }
+                done();
+            });
         });
     });
 });
